e21b105b87987c8d65e901a16a6a4ce8a30e6545
Nate Moore <nate@skypack.dev>
2021-06-29 11:25:35 -0400

    Configure netlify functions

---
+++ snowpack.config.mjs
@@ -0,0 +1,9 @@
+export default {
+    /** 
+      * This is totally optional, but kinda nice!
+      * Snowpack automatically exposes these values on `import.meta.env`
+      */
+    env: {
+        NETLIFY_URL: 'http://localhost:8888'
+    }
+}
