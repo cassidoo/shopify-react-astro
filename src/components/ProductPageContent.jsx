7784190947480ea0162d80e501b7f27f4bc467e0
cassidoo <cassidoo@users.noreply.github.com>
2021-06-30 01:50:00 -0500

    Add variants to product page

--- src/components/ProductPageContent.jsx
+++ src/components/ProductPageContent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 
 function formatPrice(num) {
   return parseFloat(num).toLocaleString('en-US', {
@@ -7,10 +7,66 @@ function formatPrice(num) {
   })
 }
 
-export default function ProductPageContent({ product }) {
-  let image = product.images.edges[0].node
+function getCurrentVariantObject(vars, id) {
+  return vars.filter((v) => {
+    return v.node.id === id
+  })[0]
+}
 
-  console.log(product)
+function VariantForm({ vars, current, pick, setQ }) {
+  return (
+    <form>
+      {vars.length > 1 &&
+        vars.map((v, index) => {
+          return (
+            <Fragment key={index}>
+              <label>
+                <input
+                  name="Product Variant"
+                  type="radio"
+                  id={v.node.id}
+                  defaultChecked={index === 0}
+                  onChange={() => {
+                    pick(v.node.id)
+                  }}
+                />
+                {v.node.title}
+              </label>
+              <br />
+            </Fragment>
+          )
+        })}
+      <input
+        type="number"
+        placeholder="Quantity"
+        defaultValue={1}
+        min={1}
+        max={getCurrentVariantObject(vars, current).node.quantityAvailable}
+        onChange={(e) => {
+          setQ(e.target.value)
+        }}
+      />
+    </form>
+  )
+}
+
+export default function ProductPageContent({ product }) {
+  let vars = product.variants.edges
+
+  const [chosenVariant, setChosenVariant] = useState(vars[0].node.id)
+  const [quantity, setQuantity] = useState(1)
+  const [cost, setCost] = useState('')
+
+  console.log(vars)
+
+  useEffect(() => {
+    let variantPrice = getCurrentVariantObject(vars, chosenVariant).node.priceV2
+      .amount
+
+    setCost(formatPrice(variantPrice * quantity))
+  }, [chosenVariant, quantity, cost])
+
+  let image = product.images.edges[0].node
 
   return (
     <div className="product-page">
@@ -19,9 +75,21 @@ export default function ProductPageContent({ product }) {
       </div>
       <div className="product-copy">
         <h1>{product.title}</h1>
-        <h2>{formatPrice(product.priceRange.minVariantPrice.amount)}</h2>
+        <h2>{cost}</h2>
         <p>{product.description}</p>
-        <button>Add to Cart</button>
+
+        <VariantForm
+          vars={vars}
+          current={chosenVariant}
+          pick={setChosenVariant}
+          setQ={setQuantity}
+        />
+
+        {product.totalInventory > 0 ? (
+          <button>Add to Cart</button>
+        ) : (
+          <button disabled>Out of Stock</button>
+        )}
       </div>
     </div>
   )
